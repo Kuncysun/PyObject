de3f0197e48afa9dff686e63f511522a70b64d9a
1815860041 <1815860041@qq.com>
2019-01-09 14:49:12 +0800

    字典嵌套列表

---
+++ 字典嵌套列表.py
@@ -0,0 +1,12 @@
+v = {'妖猫传':['黄轩','染谷将太'],'无问西东':['章子怡','王力宏','祖峰'],'超时空同居':['雷佳音','佟丽娅'],}
+v_keylist=[]
+v_valuelist=[]
+for key,value in v.items( ):
+    v_keylist.append(key)
+    v_valuelist.append(value)
+for k in v_keylist:
+    print(k)  
+for v in v_valuelist:
+    print(v)      
+""" print(v_keylist)
+print(v_valuelist) """
